6433dabe88759fb236928c25ec610e5028180ac4
ShehabNegm <shebonegm@gmail.com>
2023-08-23 16:12:51 -0700

    fix errors for place.py

--- models/place.py
+++ models/place.py
@@ -4,11 +4,12 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Float, Integer
 from sqlalchemy import ForeignKey
 from models.city import City
-from models.user import user
+from models.user import User
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
+
     __tablename__ = 'places'
     city_id = Column(String(60),ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60),ForeignKey('users.id'), nullable=False)
