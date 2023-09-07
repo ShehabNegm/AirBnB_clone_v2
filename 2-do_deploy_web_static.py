b5e694a3c0ba62437a4d627b6a2a09c65a884e44
ShehabNegm <shebonegm@gmail.com>
2023-09-07 15:59:25 -0700

    fix errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,30 +20,20 @@ def do_deploy(archive_path):
         return False
 
     try:
-        if put(archive_path, '/tmp/').failed is True:
-            return False
+        put(archive_path, '/tmp/')
         name = archive_path[9:-4]
         t_name = name + ".tgz"
-        if run("mkdir -p /data/web_static/releases/{}"
-               .format(name)).failed is True:
-            return False
-        if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-               .format(t_name, name)).failed is True:
-            return False
-        if run("rm /tmp/{}".format(t_name)).failed is True:
-            return False
-        if run("mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}"
-               .format(name, name)).failed is True:
-            return False
-        if run("rm -rf /data/web_static/releases/{}/web_static"
-               .format(name)).failed is True:
-            return False
-        if run("rm -rf /data/web_static/current").failed is True:
-            return False
-        if run("ln -s /data/web_static/releases/{} /data/web_static/current"
-               .format(name)).failed is True:
-            return False
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+            .format(t_name, name))
+        run("rm /tmp/{}".format(t_name))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf /data/web_static/releases/{} /data/web_static/current"
+            .format(name))
         print("New version deployed")
         return True
     except BaseException:
