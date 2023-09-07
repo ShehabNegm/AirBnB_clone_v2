012ee6853c6288e546c6de52173d1f59a67119f5
ShehabNegm <shebonegm@gmail.com>
2023-09-07 14:43:30 -0700

    function to distribute an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+distributes an archive to web servers
+web_01 : 3.83.253.154
+web_02 : 52.91.133.125
+"""
+from datetime import datetime
+from fabric.api import local, run, put, env
+import os
+
+env.hosts = ['3.83.253.154', '52.91.133.125']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+
+def do_deploy(archive_path):
+    """function to distribute archive to web servers"""
+
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, '/tmp/')
+        name = archive_path[9:-4]
+        t_name = name + ".tgz"
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+            .format(t_name, name))
+        run("rm /tmp/{}".format(t_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(name))
+        print("New version deployed")
+        return True
+    except BaseException:
+        return False
